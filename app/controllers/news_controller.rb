e88034f91cb59699b95734691b02daa6fc15e05c
Ben Herring <ben_herring@hotmail.com>
2018-10-10 00:39:35 +0100

    added news controller and index

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,7 @@
 class NewsController < ApplicationController
-
   def index
-
+    url = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=991b53288eab4a508a0556e8ed1f01e8'
+    article_serialized = open(url).read
+    @articles = JSON.parse(article_serialized)
   end
 end
