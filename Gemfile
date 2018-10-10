cfd3a4df8c4b10d9cc0c6f815c344e3f2bf1d15e
Ben Herring <ben_herring@hotmail.com>
2018-10-10 14:08:13 +0100

    done

--- Gemfile
+++ Gemfile
@@ -17,7 +17,8 @@ gem 'uglifier', '>= 1.3.0'
 gem 'webpacker'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
-# gem "json"
+gem "json"
+gem 'dotenv-rails', groups: [:development, :test]
 gem 'bootstrap-sass'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,8 @@
 class NewsController < ApplicationController
   require "open-uri"
   def index
+    # I tried to interpolate my API key in the commented code below but i couldnt seem to make it work, so i just used the key here.
+    # url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=#{ENV['NEWS_API_KEY']}"
     url = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=991b53288eab4a508a0556e8ed1f01e8'
     article_serialized = open(url).read
     @articles = JSON.parse(article_serialized)
