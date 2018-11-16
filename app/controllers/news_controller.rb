a51f1bee115ffd4429176b0d41503c84fc1f5c3f
Ben Herring <ben_herring@hotmail.com>
2018-11-16 01:02:29 +0000

    removed key

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -3,7 +3,7 @@ class NewsController < ApplicationController
   def index
     # I tried to interpolate my API key in the commented code below but i couldnt seem to make it work, so i just used the key here.
     # url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=#{ENV['NEWS_API_KEY']}"
-    url = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=991b53288eab4a508a0556e8ed1f01e8'
+    url = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='
     article_serialized = open(url).read
     @articles = JSON.parse(article_serialized)
   end
